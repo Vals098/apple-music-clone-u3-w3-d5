853f0c23c97db500216145361afaa892dbf3b859
Vals098 <valeria.farinosi@gmail.com>
2026-05-15 16:21:43 +0200

    add: base player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,25 @@
-const Player = function(){
-    return <div className="bg-secondary text-light d-flex align-items-center px-3" style={{height: "50px"}}>Player Bar</div>
+const Player = function () {
+  return (
+    <div className="player-bar d-flex justify-content-between align-items-center px-4 text-secondary">
+      {/* LEFT */}
+      <div className="d-flex align-items-center gap-3">
+        <img
+          
+        />
+
+        <div>
+          <p>Titolo</p>
+          <small>Artista</small>
+        </div>
+      </div>
+
+      {/* CENTER */}
+      <div>controls</div>
+
+      {/* RIGHT */}
+      <div>volume</div>
+    </div>
+  )
 }
 
 export default Player
