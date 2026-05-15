afe3de1da6cfb9127e2fb59ecd89670e22fce475
Vals098 <valeria.farinosi@gmail.com>
2026-05-15 13:12:32 +0200

    update: RadioCard layout

--- src/components/RadioCard.jsx
+++ src/components/RadioCard.jsx
@@ -1,8 +1,8 @@
 const RadioCard = function ({ title, image }) {
   return (
-    <div>
-      <p className="fw-bold text-secondary">NUOVA STAZIONE RADIO</p>
-      <h3>{title}</h3>
+    <div className="radio-card">
+      <p className="fw-bold text-secondary mb-0">NUOVA STAZIONE RADIO</p>
+      <h3 className="fs-4 fw-light">{title}</h3>
       <img src={image} className="img-fluid flex-fill rounded-4"/>
     </div>
   )
