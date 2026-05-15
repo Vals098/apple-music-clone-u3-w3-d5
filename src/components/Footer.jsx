9692bfd441fefe8d0f93dd8833e0e91a1c573c8b
Vals098 <valeria.farinosi@gmail.com>
2026-05-15 16:14:52 +0200

    add: footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+const Footer = function () {
+  return (
+    <footer className="mt-5 p-4 pb-5 text-white-50 fw-light small">
+      <div className="d-flex gap-3 mb-4 flex-wrap">
+        <span>Italia</span>
+        <span>English (UK)</span>
+      </div>
+      <p className="mb-3">
+        Copyright © 2024 Apple Inc. Tutti i diritti riservati.
+      </p>
+      <div className="d-flex gap-3 flex-wrap">
+        <span>Condizioni dei servizi internet</span>
+        <span>Apple Music e privacy</span>
+        <span>Avviso sui cookie</span>
+        <span>Supporto</span>
+        <span>Feedback</span>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import ExploreSection from "./ExploreSection"
+import Footer from "./Footer"
 import HeroSection from "./HeroSection"
 import NavBar from "./NavBar"
 import RadioEpisodesSection from "./RadioEpisodesSection"
@@ -8,14 +9,17 @@ const Home = function () {
   return (
     <div className="h-100 d-flex flex-column bg-black text-light">
       <NavBar />
-      <div className="flex-grow-1 overflow-auto p-4">
-        <HeroSection />
-        <RadioEpisodesSection />
-        <h2 className="mt-4">Nuove uscite</h2>
-        <SongsSection title="Edoardo Bennato" query="edoardo-bennato" />
-        <SongsSection title="883" query="883" />
-        <SongsSection title="Queen" query="queen" />
-        <ExploreSection/>
+      <div className="flex-grow-1 overflow-auto">
+        <div className="p-4">
+          <HeroSection />
+          <RadioEpisodesSection />
+          <h2 className="mt-4">Nuove uscite</h2>
+          <SongsSection title="Edoardo Bennato" query="edoardo-bennato" />
+          <SongsSection title="883" query="883" />
+          <SongsSection title="Queen" query="queen" />
+          <ExploreSection />
+        </div>
+        <Footer />
       </div>
     </div>
   )
