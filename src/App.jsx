7546b03dc789f32fab294a4ac7400810113c2f12
Vals098 <valeria.farinosi@gmail.com>
2026-05-15 15:37:41 +0200

    add: fixed scroll

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,17 @@ import SideBar from "./components/SideBar"
 
 function App() {
   return (
-   <Container fluid className="vh-100 d-flex flex-column px-0">
-      {/* <div className="d-flex flex-grow-1"> */}
-      <Row className="flex-grow-1 g-0">
-        <Col md={3} lg={2} className="d-none d-md-block">
-          <SideBar className="d-none d-md-block" />
+    <Container fluid className="vh-100 d-flex flex-column px-0">
+      <Row className="flex-grow-1 g-0 overflow-hidden">
+        <Col md={3} lg={2} className="d-none d-md-block h-100">
+          <SideBar />
         </Col>
-        <Col xs={12} md={9} lg={10}>
+
+        <Col xs={12} md={9} lg={10} className="h-100">
           <Home />
         </Col>
-        {/* </div> */}
       </Row>
+
       <Player />
     </Container>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,15 +5,15 @@ import SongsSection from "./SongsSection"
 
 const Home = function () {
   return (
-    <div className="flex-grow-1 bg-black text-light overflow-auto h-100">
-        <NavBar />
-      <div className="p-4">
-      <HeroSection/>
-      <RadioEpisodesSection/>
-      <h2 className="mt-4">Nuove uscite</h2>
-      <SongsSection title="Edoardo Bennato" query="edoardo-bennato"/>
-      <SongsSection title="883" query="883"/>
-      <SongsSection title="Queen" query="queen"/>
+    <div className="h-100 d-flex flex-column bg-black text-light">
+      <NavBar />
+      <div className="flex-grow-1 overflow-auto p-4">
+        <HeroSection />
+        <RadioEpisodesSection />
+        <h2 className="mt-4">Nuove uscite</h2>
+        <SongsSection title="Edoardo Bennato" query="edoardo-bennato" />
+        <SongsSection title="883" query="883" />
+        <SongsSection title="Queen" query="queen" />
       </div>
     </div>
   )
